6b4d4c12736252a4de62547bc5dc97221541bd5b
chrisbrydges <82475396+chrisbrydges@users.noreply.github.com>
2022-03-23 18:38:45 -0700

    Add files via upload

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(shinydashboard)
 library(shiny)
 library(DT)
 library(factoextra)
+library(dplyr)
 load("county_data.RData")
 
 ui <- dashboardPage(skin = "green",
@@ -21,7 +22,8 @@ ui <- dashboardPage(skin = "green",
                                           "Race (% Asian)" = "race_asian",
                                           "Race (% Hawaiian/Pacific Island)" = "race_hawai",
                                           "Race (% Other)" = "race_other",
-                                          "Race (% Multi)" = "race_multi")),
+                                          "Race (% Multi)" = "race_multi",
+                                          "2020 Election Result" = "votes_DEM")),
                      actionButton("go", "Find matches")),
     dashboardBody(
         DT::dataTableOutput("similar")
@@ -33,8 +35,10 @@ server <- function(input, output) {
     output$similar <- DT::renderDataTable({
         input$go
         isolate({
+            # What are the selected variables
+            vars_of_interest <- input$variable
             # Scale selected variables
-            x    <- scale(df[, input$variable])
+            x <- scale(df[, vars_of_interest])
             rownames(x) <- df$NAME
             # Calculate distances for every county
             res.dist <- as.matrix(dist(x, method = "euclidean"))
@@ -46,31 +50,34 @@ server <- function(input, output) {
             # Put them in rank order
             df2$Rank <- rank(df2$Distance, ties.method = "average")
             df2$Rank <- df2$Rank - 1
+            # Replace the vote_DEM variable with 2020 Election Voting
+            vars_of_interest <- ifelse(vars_of_interest == "votes_DEM", "VOTE", vars_of_interest)
             # Put the demographic data into a table with the distance and rank
             df2 <- merge(df2, df, by.x = "County", by.y = "NAME")
-            df2 <- df2[, (colnames(df2) %in% c("County", "Rank", "Distance", "total_population", "age",
-                                               "income", "education_bachelors", "health_uninsured", 
-                                               "hispanic", "race_white", "race_black", "race_natam", 
-                                               "race_asian", "race_hawai", "race_other", "race_multi"))]
+            df2 <- df2[, (colnames(df2) %in% c("County", "Rank", "Distance", vars_of_interest))]
             df2 <- df2[order(df2$Rank), ]
             
-            # Round all the numberic variables to two decimal places
-            df2$Distance <- round(df2$Distance, 2)
-            df2$education_bachelors <- round(df2$education_bachelors, 2)
-            df2$health_uninsured <- round(df2$health_uninsured, 2)
-            df2$hispanic <- round(df2$hispanic, 2)
-            df2$race_white <- round(df2$race_white, 2)
-            df2$race_black <- round(df2$race_black, 2)
-            df2$race_natam <- round(df2$race_natam, 2)
-            df2$race_asian <- round(df2$race_asian, 2)
-            df2$race_hawai <- round(df2$race_hawai, 2)
-            df2$race_other <- round(df2$race_other, 2)
-            df2$race_multi <- round(df2$race_multi, 2)
-            colnames(df2) <- c("County", "Difference", "Rank", "Population", "Median Age",
-                               "Median Income", "Education (% Bachelor's degree or more)",
-                               "Health (% uninsured)", "% Hispanic", "Race (% White)",
-                               "Race (% Black)", "Race (% Native American)", "Race (% Asian)", 
-                               "Race (% Hawaiian/Pacific Island)", "Race (% Other)", "Race (% Multi)")
+            # Round all the numeric variables to two decimal places
+            Round <- function(x, k) if (is.numeric(x)) round(x, k) else x
+            replace(df2, TRUE, lapply(df2, Round, 2))
+            
+            # Rename columns to be more readable
+            lookup <- c("Population" = "total_population",
+                        "Median Age" = "age",
+                        "Median Income" = "income",
+                        "Education (% Bachelor's degree or more)" = "education_bachelors",
+                        "Health (% uninsured)" = "health_uninsured", 
+                        "% Hispanic" = "hispanic",
+                        "Race (% White)" = "race_white",
+                        "Race (% Black)" = "race_black",
+                        "Race (% Native American)" = "race_natam",
+                        "Race (% Asian)" = "race_asian",
+                        "Race (% Hawaiian/Pacific Island)" = "race_hawai",
+                        "Race (% Other)" = "race_other",
+                        "Race (% Multi)" = "race_multi",
+                        "2020 Election Result" = "VOTE")
+            
+            df2 <- df2 %>% rename(any_of(lookup))
             
             # Create the table
             df2})
